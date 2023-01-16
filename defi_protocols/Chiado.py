edf2cd2c632fbe2b670591582641174b660267ab
dharmendra kariya <dharamendra.kariya@gmail.com>
2023-01-17 03:06:09 +0530

    Tests: adding env test

--- defi_protocols/Chiado.py
+++ defi_protocols/Chiado.py
@@ -2,6 +2,8 @@ from defi_protocols.functions import *
 
 from defi_protocols.test_env import *
 
+CHIADO = "chiado"
+
 def get_chiado_rpc(blockchain):
   if blockchain == CHIADO:
       return NODE_CHIADO
