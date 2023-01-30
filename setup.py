6d21b838f5e898ed47e4efde752a072de291031c
dharmendra kariya <dharamendra.kariya@gmail.com>
2023-01-31 03:36:48 +0530

    Update: package

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 setuptools.setup(
     name="defi_protocols",
-    version="0.0.2",
+    version="0.0.3",
     author="dharmendrakariya",
     author_email="dharmendra@karpatkey.com",
     description="A simple defi_protocols package",
