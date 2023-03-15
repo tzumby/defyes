3240437cff890170cd3ed43ed544e95f0fb8cc12
Santiago Piccinini <san@ektocomms.space>
2023-03-15 20:39:49 -0300

    Add more sushiswap tests

--- tests/test_sushiswap.py
+++ tests/test_sushiswap.py
@@ -1,5 +1,6 @@
 from defi_protocols import SushiSwap
 from defi_protocols.constants import ETHEREUM, USDC_ETH, WETH_ETH
+from defi_protocols.functions import get_node
 
 
 SUSHISWAP_POOL_USDC_WETH = '0x397FF1542f962076d0BFE58eA045FfA2d347ACa0'
@@ -14,3 +15,24 @@ def test_get_lptoken_data():
 def test_underlying_of_empty_address():
     data = SushiSwap.underlying(UNUSED_ADDRESS, SUSHISWAP_POOL_USDC_WETH, 'latest', ETHEREUM)
     assert data == [[USDC_ETH, 0.0, 0.0], [WETH_ETH, 0.0, 0.0]]
+
+def test_get_pool_info_v1():
+    web3 = get_node(ETHEREUM)
+    data = SushiSwap.get_pool_info(web3, SUSHISWAP_POOL_USDC_WETH, block=16836190,
+                                   blockchain=ETHEREUM, use_db=True)
+    assert data['pool_info'] == {'poolId': 1, 'allocPoint': 8300}
+    assert data['totalAllocPoint'] == 1639550
+
+def test_get_pool_info_v2():
+    web3 = get_node(ETHEREUM)
+    data = SushiSwap.get_pool_info(web3, '0x269Db91Fc3c7fCC275C2E6f22e5552504512811c', block=16836190,
+                                   blockchain=ETHEREUM, use_db=True)
+    assert data['pool_info'] == {'poolId': 3, 'allocPoint': 5}
+    assert data['totalAllocPoint'] == 1125
+
+def test_get_pool_info_v2_without_db():
+    web3 = get_node(ETHEREUM)
+    data = SushiSwap.get_pool_info(web3, '0x269Db91Fc3c7fCC275C2E6f22e5552504512811c', block=16836190,
+                                   blockchain=ETHEREUM, use_db=False)
+    assert data['pool_info'] == {'poolId': 3, 'allocPoint': 5}
+    assert data['totalAllocPoint'] == 1125
