ffd20cb8abc4f2c4522e11d2a2a1a421e0e1f15d
franalbani <franalbani@users.noreply.github.com>
2023-04-18 13:53:40 -0300

    replace maker tests vault and wallet

--- tests/test_maker.py
+++ tests/test_maker.py
@@ -7,31 +7,31 @@ from defi_protocols.constants import ETHEREUM, DAI_ETH, WETH_ETH, ETHTokenAddr
 TEST_BLOCK = 17070386
 WEB3 = get_node(blockchain=ETHEREUM, block=TEST_BLOCK)
 # TEST_WALLET = '0xf929122994e177079c924631ba13fb280f5cd1f9'
-TEST_WALLET = '0x849D52316331967b6fF1198e5E32A0eB168D039d'
-VAULT_ID = 10
+# TEST_WALLET = '0x849D52316331967b6fF1198e5E32A0eB168D039d'
+TEST_WALLET = '0x4971DD016127F390a3EF6b956Ff944d0E2e1e462'
+VAULT_ID = 27353
 
 
 def test_get_vault_data():
     x = Maker.get_vault_data(VAULT_ID, TEST_BLOCK, WEB3)
-    assert x == {'mat': 1.45,
-                 'gem': WETH_ETH,
+    assert x == {'mat': 1.6,
+                 'gem': ETHTokenAddr.stETH,
                  'dai': DAI_ETH, 
-                 'ink': 0.0,
-                 'art': 0.0,
-                 'Art': 213059126.61267862,
-                 'rate': 1.0877561923001058,
-                 'spot': 1434.1094163,
-                 'line': 421276896.85485435,
+                 'ink': 57328.918780519,
+                 'art': 21811755.174275193,
+                 'Art': 131281671.56044462,
+                 'rate': 1.0337823922958922,
+                 'spot': 1456.9286150664384,
+                 'line': 154522941.83599702,
                  'dust': 7500.0}
 
 
 def test_underlying():
     x = Maker.underlying(VAULT_ID, TEST_BLOCK, WEB3)
-    assert x == [[WETH_ETH, 0.0],
-                 [DAI_ETH, 0.0]]
-
+    assert x == [[ETHTokenAddr.stETH, 57328.918780519],
+                 [DAI_ETH, -22548608.444234513]]
 
 def test_get_delegated_MKR():
     x = Maker.get_delegated_MKR(TEST_WALLET, TEST_BLOCK, WEB3,
                                 decimals=False)
-    assert x == [[ETHTokenAddr.MKR, 0]]
+    assert x == [[ETHTokenAddr.MKR, 583805204609736124092]]
