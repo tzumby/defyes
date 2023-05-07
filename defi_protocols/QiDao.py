892bb9e8f600e7882df7b41822f841b0a1c03419
fito <31478879+ph1t0@users.noreply.github.com>
2023-05-07 13:00:16 -0300

    adds some tests for QiDAO along with some refactoring

--- defi_protocols/QiDao.py
+++ defi_protocols/QiDao.py
@@ -1,30 +1,24 @@
-from defi_protocols.functions import get_node, get_contract, get_decimals, timestamp_to_block, block_to_timestamp, GetNodeIndexError
-from defi_protocols.constants import XDAI, GNO_XDAI, WETH_XDAI, POLYGON, MAX_EXECUTIONS, MAI_POL
+from decimal import Decimal
+
+from defi_protocols.functions import get_node, get_contract, get_decimals, timestamp_to_block, block_to_timestamp
+from defi_protocols.constants import XDAI, GnosisTokenAddr, POLYGON, MAI_POL
 
 # ---------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 # QIDAO_VAULTS
 # ---------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 # QiDao Vaults List
-QIDAO_VAULTS = [
+QIDAO_VAULTS = {
+    GnosisTokenAddr.GNO: {
+        'blockchain': XDAI,
+        'address': '0x014A177E9642d1b4E970418f894985dC1b85657f'  # GNO Vault Address
+        },
+    GnosisTokenAddr.WETH:
     {
         'blockchain': XDAI,
-        'vaults': [
-            {
-                'collateral': GNO_XDAI,
-                'address': '0x014A177E9642d1b4E970418f894985dC1b85657f'  # GNO Vault Address
-            },
-            {
-                'collateral': WETH_XDAI,
-                'address': '0x5c49b268c9841AFF1Cc3B0a418ff5c3442eE3F3b'  # WETH Vault Address
-            }
-        ]
-    },
-
-    {
-        'blockchain': POLYGON,
-        'vaults': []
+        'address': '0x5c49b268c9841AFF1Cc3B0a418ff5c3442eE3F3b'  # WETH Vault Address
     }
-]
+}
+
 
 # 1inch Polygon Oracle Address
 ORACLE_1INCH_POLYGON = '0x7F069df72b7A39bCE9806e3AfaF579E54D8CF2b9'
@@ -50,24 +44,16 @@ ABI_CHAINLINK_PRICE_FEED = '[{"inputs":[],"name":"latestAnswer","outputs":[{"int
 # ---------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 def get_vault_address(collateral_address, blockchain):
     """
-
     :param collateral_address:
     :param blockchain:
     :return:
     """
     vault_address = None
-
-    for qidao_vault in QIDAO_VAULTS:
-
-        if qidao_vault['blockchain'] == blockchain:
-
-            for vault in qidao_vault['vaults']:
-
-                if vault['collateral'] == collateral_address:
-                    vault_address = vault['address']
-                    break
-
-            break
+    try:
+        if QIDAO_VAULTS[collateral_address]['blockchain'] == blockchain:
+            vault_address = QIDAO_VAULTS[collateral_address]['address']
+    except KeyError:
+        pass
 
     return vault_address
 
@@ -79,131 +65,98 @@ def get_vault_address(collateral_address, blockchain):
 # 'web3' = web3 (Node) -> Improves performance
 # 'decimals' = True -> retrieves the results considering the decimals / 'decimals' = False or not passed onto the function -> decimals are not considered
 # ---------------------------------------------------------------------------------------------------------------------------------------------------------------------------
-def get_vault_data(vault_id, collateral_address, block, blockchain, web3=None, execution=1, index=0, decimals=True):
+def get_vault_data(vault_id, collateral_address, block, blockchain, web3=None, decimals=True):
     """
-
     :param vault_id:
     :param collateral_address:
     :param block:
     :param blockchain:
     :param web3:
-    :param execution:
-    :param index:
     :param decimals:
     :return:
     """
-    # If the number of executions is greater than the MAX_EXECUTIONS variable -> returns None and halts
-    if execution > MAX_EXECUTIONS:
-        return None
-
     vault_data = {}
 
-    try:
-        if web3 is None:
-            web3 = get_node(blockchain, block=block)
+    if web3 is None:
+        web3 = get_node(blockchain, block=block)
 
-        collateral_address = web3.to_checksum_address(collateral_address)
+    collateral_address = web3.to_checksum_address(collateral_address)
 
-        vault_address = get_vault_address(collateral_address, blockchain)
+    vault_address = get_vault_address(collateral_address, blockchain)
 
-        if vault_address is not None:
-            vault_contract = get_contract(vault_address, blockchain, web3=web3, abi=ABI_VAULT, block='latest')
+    if vault_address is not None:
+        vault_contract = get_contract(vault_address, blockchain, web3=web3, abi=ABI_VAULT, block='latest')
 
-            if vault_contract.functions.exists(vault_id).call(block_identifier=block):
+        if vault_contract.functions.exists(vault_id).call(block_identifier=block):
 
-                debt_address = vault_contract.functions.mai().call()
+            debt_address = vault_contract.functions.mai().call()
+            vault_collateral = vault_contract.functions.vaultCollateral(vault_id).call(block_identifier=block)
+            vault_debt = vault_contract.functions.vaultDebt(vault_id).call(block_identifier=block)
 
-                debt_decimals = get_decimals(debt_address, blockchain, web3=web3)
-                collateral_decimals = get_decimals(collateral_address, blockchain, web3=web3)
+            debt_decimals = get_decimals(debt_address, blockchain, web3=web3) if decimals else 0
+            collateral_decimals = get_decimals(collateral_address, blockchain, web3=web3) if decimals else 0
+            price_source_decimals = vault_contract.functions.priceSourceDecimals().call()
 
-                vault_collateral = vault_contract.functions.vaultCollateral(vault_id).call(block_identifier=block)
-                vault_debt = vault_contract.functions.vaultDebt(vault_id).call(block_identifier=block)
+            # Collateral Address
+            vault_data['collateral_address'] = collateral_address
+            # Collateral Amount
+            vault_data['collateral_amount'] = Decimal(vault_collateral) / Decimal(10 ** collateral_decimals)
+            # Collateral Token USD Value
+            vault_data['collateral_token_usd_value'] = Decimal(vault_contract.functions.getEthPriceSource().call(block_identifier=block))
+            vault_data['collateral_token_usd_value'] /= Decimal(10 ** price_source_decimals)
+            # Debt Address
+            vault_data['debt_address'] = debt_address
+            # Debt Amount
+            vault_data['debt_amount'] = Decimal(vault_debt) / Decimal(10 ** debt_decimals)
 
-                price_source_decimals = vault_contract.functions.priceSourceDecimals().call()
+            # Debt Token USD Value
+            # getTokenPriceSource() always returns MAI price = 1 USD. This is the price QiDao uses to calculate the Collateral Ratio.
+            # MAI price might have depegged from USD so afterwards vault_data['debt_token_usd_value'] is overwritten with the price obtained from 1inch
+            vault_data['debt_token_usd_value'] = Decimal(vault_contract.functions.getTokenPriceSource().call(block_identifier=block))
+            vault_data['debt_token_usd_value'] /= Decimal(10 ** price_source_decimals)
 
-                # Collateral Address
-                vault_data['collateral_address'] = collateral_address
+            # Debt USD Value
+            if vault_debt != 0:
+                vault_data['debt_usd_value'] = vault_data['debt_token_usd_value'] * Decimal(vault_debt) / Decimal(10 ** debt_decimals)
+            else:
+                vault_data['debt_usd_value'] = Decimal(0)
 
-                # Collateral Amount
-                if decimals is True:
-                    vault_data['collateral_amount'] = vault_collateral / (10 ** collateral_decimals)
-                else:
-                    vault_data['collateral_amount'] = vault_collateral
+            # Collateral Ratio
+            if vault_debt != 0:
+                vault_collateral_dec = Decimal(vault_collateral) / Decimal(10 ** collateral_decimals)
+                vault_data['collateral_ratio'] = vault_collateral_dec * vault_data['collateral_token_usd_value']
+                vault_data['collateral_ratio'] /= vault_data['debt_usd_value'] * 100
+            else:
+                vault_data['collateral_ratio'] = None
 
-                # Collateral Token USD Value
-                vault_data['collateral_token_usd_value'] = vault_contract.functions.getEthPriceSource().call(
-                    block_identifier=block) / (10 ** price_source_decimals)
+            # Available Debt Amount to Borrow
+            vault_data['available_debt_amount'] = Decimal(vault_contract.functions.getDebtCeiling().call(block_identifier=block))
+            vault_data['available_debt_amount'] /= Decimal(10 ** debt_decimals)
 
-                # Debt Address
-                vault_data['debt_address'] = debt_address
+            # Liquidation Ratio
+            vault_data['liquidation_ratio'] = vault_contract.functions._minimumCollateralPercentage().call(block_identifier=block)
+            # Liquidation Price
+            if vault_debt != 0:
+                vault_collateral_dec = Decimal(vault_collateral) / Decimal(10 ** collateral_decimals)
+                vault_data['liquidation_price'] = Decimal(vault_data['liquidation_ratio'] / 100) * vault_data['debt_usd_value'] / vault_collateral_dec
+            else:
+                vault_data['liquidation_price'] = None
 
-                # Debt Amount
-                if decimals is True:
-                    vault_data['debt_amount'] = vault_debt / (10 ** debt_decimals)
-                else:
-                    vault_data['debt_amount'] = vault_debt
+            # Debt Token USD Value from Polygon Chainlink feed
+            block_polygon = timestamp_to_block(block_to_timestamp(block, blockchain), POLYGON)
 
-                # Debt Token USD Value
-                # getTokenPriceSource() always returns MAI price = 1 USD. This is the price QiDao uses to calculate the Collateral Ratio.
-                # MAI price might have depegged from USD so afterwards vault_data['debt_token_usd_value'] is overwritten with the price obtained from 1inch
-                vault_data['debt_token_usd_value'] = vault_contract.functions.getTokenPriceSource().call(
-                    block_identifier=block) / (10 ** price_source_decimals)
+            price_feed_contract = get_contract(CHAINLINK_MATIC_USD, POLYGON, abi=ABI_CHAINLINK_PRICE_FEED, block=block_polygon)
+            price_feed_decimals = price_feed_contract.functions.decimals().call()
+            matic_usd_price = Decimal(price_feed_contract.functions.latestAnswer().call(block_identifier=block_polygon))
+            matic_usd_price /= Decimal(10 ** price_feed_decimals)
 
-                # Debt USD Value
-                if vault_debt != 0:
-                    vault_data['debt_usd_value'] = (vault_debt / (10 ** debt_decimals)) * vault_data[
-                        'debt_token_usd_value']
-                else:
-                    vault_data['debt_usd_value'] = 0
+            oracle_contract = get_contract(ORACLE_1INCH_POLYGON, POLYGON, abi=ABI_ORACLE, block=block_polygon)
+            rate = Decimal(oracle_contract.functions.getRateToEth(MAI_POL, False).call(block_identifier=block_polygon))
+            rate /= Decimal(10 ** abs(18 + 18 - debt_decimals))
 
-                # Collateral Ratio
-                if vault_debt != 0:
-                    vault_data['collateral_ratio'] = (((vault_collateral / (10 ** collateral_decimals)) * vault_data[
-                        'collateral_token_usd_value']) / vault_data['debt_usd_value']) * 100
-                else:
-                    vault_data['collateral_ratio'] = None
+            vault_data['debt_token_usd_value'] = matic_usd_price * rate
 
-                # Available Debt Amount to Borrow
-                if decimals is True:
-                    vault_data['available_debt_amount'] = vault_contract.functions.getDebtCeiling().call(
-                        block_identifier=block) / (10 ** debt_decimals)
-                else:
-                    vault_data['available_debt_amount'] = vault_contract.functions.getDebtCeiling().call(
-                        block_identifier=block)
-
-                # Liquidation Ratio
-                vault_data['liquidation_ratio'] = vault_contract.functions._minimumCollateralPercentage().call(
-                    block_identifier=block)
-
-                # Liquidation Price
-                if vault_debt != 0:
-                    vault_data['liquidation_price'] = ((vault_data['liquidation_ratio'] / 100) * vault_data[
-                        'debt_usd_value']) / (vault_collateral / (10 ** collateral_decimals))
-                else:
-                    vault_data['liquidation_price'] = None
-
-                # Debt Token USD Value from Polygon Chainlink feed
-                block_polygon = timestamp_to_block(block_to_timestamp(block, blockchain), POLYGON)
-                price_feed_contract = get_contract(CHAINLINK_MATIC_USD, POLYGON, abi=ABI_CHAINLINK_PRICE_FEED,
-                                                   block=block_polygon)
-                price_feed_decimals = price_feed_contract.functions.decimals().call()
-                matic_usd_price = price_feed_contract.functions.latestAnswer().call(block_identifier=block_polygon) / (
-                            10 ** price_feed_decimals)
-
-                oracle_contract = get_contract(ORACLE_1INCH_POLYGON, POLYGON, abi=ABI_ORACLE, block=block_polygon)
-                rate = oracle_contract.functions.getRateToEth(MAI_POL, False).call(block_identifier=block_polygon) / (
-                            10 ** abs(18 + 18 - debt_decimals))
-                vault_data['debt_token_usd_value'] = matic_usd_price * rate
-
-        return vault_data
-
-    except GetNodeIndexError:
-        return get_vault_data(vault_id, collateral_address, block, blockchain, decimals=decimals, index=0,
-                              execution=execution + 1)
-
-    except:
-        return get_vault_data(vault_id, collateral_address, block, blockchain, decimals=decimals, index=index + 1,
-                              execution=execution)
+    return vault_data
 
 
 # ---------------------------------------------------------------------------------------------------------------------------------------------------------------------------
@@ -215,66 +168,42 @@ def get_vault_data(vault_id, collateral_address, block, blockchain, web3=None, e
 # Output:
 # 1 - Tuple: [[collateral_address, collateral_amount], [debt_address, -debt_amount]]
 # ---------------------------------------------------------------------------------------------------------------------------------------------------------------------------
-def underlying(vault_id, collateral_address, block, blockchain, web3=None, execution=1, index=0, decimals=True):
+def underlying(vault_id, collateral_address, block, blockchain, web3=None, decimals=True):
     """
-
     :param vault_id:
     :param collateral_address:
     :param block:
     :param blockchain:
     :param web3:
-    :param execution:
-    :param index:
     :param decimals:
     :return:
     """
-    # If the number of executions is greater than the MAX_EXECUTIONS variable -> returns None and halts
-    if execution > MAX_EXECUTIONS:
-        return None
-
     result = []
 
-    try:
-        if web3 is None:
-            web3 = get_node(blockchain, block=block)
+    if web3 is None:
+        web3 = get_node(blockchain, block=block)
 
-        collateral_address = web3.to_checksum_address(collateral_address)
+    collateral_address = web3.to_checksum_address(collateral_address)
+    vault_address = get_vault_address(collateral_address, blockchain)
 
-        vault_address = get_vault_address(collateral_address, blockchain)
+    if vault_address is not None:
+        vault_contract = get_contract(vault_address, blockchain, web3=web3, abi=ABI_VAULT, block=block)
 
-        if vault_address is not None:
-            vault_contract = get_contract(vault_address, blockchain, web3=web3, abi=ABI_VAULT, block=block)
+        if vault_contract.functions.exists(vault_id).call(block_identifier=block):
 
-            if vault_contract.functions.exists(vault_id).call(block_identifier=block):
+            collateral_decimals = get_decimals(collateral_address, blockchain, web3=web3) if decimals else 0
 
-                if decimals is True:
-                    collateral_decimals = get_decimals(collateral_address, blockchain, web3=web3)
-                else:
-                    collateral_decimals = 0
+            collateral_amount = Decimal(vault_contract.functions.vaultCollateral(vault_id).call(block_identifier=block))
+            collateral_amount /= Decimal(10 ** collateral_decimals)
 
-                collateral_amount = vault_contract.functions.vaultCollateral(vault_id).call(block_identifier=block) / (
-                            10 ** collateral_decimals)
+            result.append([collateral_address, collateral_amount])
 
-                result.append([collateral_address, collateral_amount])
+            debt_address = vault_contract.functions.mai().call()
+            debt_decimals = get_decimals(debt_address, blockchain, web3=web3) if decimals else 0
 
-                debt_address = vault_contract.functions.mai().call()
+            debt_amount = -1 * Decimal(vault_contract.functions.vaultDebt(vault_id).call(block_identifier=block))
+            debt_amount /= Decimal(10 ** debt_decimals)
 
-                if decimals is True:
-                    debt_decimals = get_decimals(debt_address, blockchain, web3=web3)
-                else:
-                    debt_decimals = 0
+            result.append([debt_address, debt_amount])
 
-                debt_amount = -1 * vault_contract.functions.vaultDebt(vault_id).call(block_identifier=block) / (
-                            10 ** debt_decimals)
-
-                result.append([debt_address, debt_amount])
-
-        return result
-
-    except GetNodeIndexError:
-        return underlying(vault_id, collateral_address, block, blockchain, decimals=decimals, index=0,
-                          execution=execution + 1)
-
-    except:
-        return underlying(vault_id, collateral_address, block, blockchain, decimals=decimals, index=index + 1,
-                          execution=execution)
+    return result
---
+++ tests/test_qidao.py
@@ -0,0 +1,26 @@
+from decimal import Decimal
+
+from defi_protocols import QiDao
+from defi_protocols.constants import GnosisTokenAddr, XDAI
+
+
+def test_get_vaul_address():
+    address = QiDao.get_vault_address(GnosisTokenAddr.WETH, XDAI)
+    assert address == '0x5c49b268c9841AFF1Cc3B0a418ff5c3442eE3F3b'
+
+
+def test_get_vault_data():
+    block = 27814350
+    data = QiDao.get_vault_data(0, GnosisTokenAddr.GNO, block, XDAI)
+    assert data == {'collateral_address': GnosisTokenAddr.GNO, 'collateral_amount': Decimal('12.669153514705549101'),
+                    'collateral_token_usd_value': Decimal('115.44042'),
+                    'debt_address': GnosisTokenAddr.MAI, 'debt_amount': Decimal('0'),
+                    'debt_token_usd_value': Decimal('0.99446664920316931668708736'), 'debt_usd_value': Decimal('0'),
+                    'collateral_ratio': None, 'available_debt_amount': Decimal('3259.913205994382808393'),
+                    'liquidation_ratio': 130, 'liquidation_price': None}
+
+
+def test_underlying():
+    block = 27814350
+    underlying = QiDao.underlying(0, GnosisTokenAddr.GNO, block, XDAI)
+    assert underlying == [[GnosisTokenAddr.GNO, Decimal('12.669153514705549101')], [GnosisTokenAddr.MAI, Decimal('-0')]]
