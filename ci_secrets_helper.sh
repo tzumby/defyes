4f00bc056fcc9f5b3d9e6bf36559408e5ee1e5ca
franalbani <franalbani@users.noreply.github.com>
2023-05-12 17:36:19 -0300

    fixes ci_secrets_helper

--- ci_secrets_helper.sh
+++ ci_secrets_helper.sh
@@ -1,8 +1,8 @@
-JQ="      .nodes.ethereum.latest   = \"${NODE_ETH}\""
-JQ=$JQ" | .nodes.ethereum.archival = \"${NODE_ETH}\""
-JQ=$JQ" | .nodes.xdai.archival     = \"${NODE_XDAI}\""
-JQ=$JQ" | .nodes.polygon.archival  = \"${NODE_POLYGON}\""
-JQ=$JQ" | .nodes.optimism.archival = \"${NODE_OPTIMISM}\""
+JQ="      .nodes.ethereum.latest   = [\"${NODE_ETH}\"]"
+JQ=$JQ" | .nodes.ethereum.archival = [\"${NODE_ETH}\"]"
+JQ=$JQ" | .nodes.xdai.archival     = [\"${NODE_XDAI}\"]"
+JQ=$JQ" | .nodes.polygon.archival  = [\"${NODE_POLYGON}\"]"
+JQ=$JQ" | .nodes.optimism.archival = [\"${NODE_OPTIMISM}\"]"
 JQ=$JQ" | .apikeys.etherscan       = \"${APIKEYS_ETHERSCAN}\""
 JQ=$JQ" | .apikeys.polscan         = \"${APIKEYS_POLSCAN}\""
 echo $JQ 
