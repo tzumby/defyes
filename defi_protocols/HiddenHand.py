588386065986bc30af51da569fbdc50509499b53
Richymaestro <xxxx@yyy.com>
2023-05-17 09:49:48 +0200

    Hidden Hand underlying all fix Fixes #139

--- defi_protocols/HiddenHand.py
+++ defi_protocols/HiddenHand.py
@@ -1,5 +1,6 @@
 import requests
-from typing import List, Union, Optional
+from typing import List, Union, Dict
+from decimal import Decimal
 
 def get_api_results(wallet: str, blockchain: str) -> Union[List[dict], str]:
     if blockchain == 'ethereum':
@@ -19,18 +20,29 @@ def underlying(wallet: str, token: str, blockchain: str, decimals: bool = True) 
     return [x for x in result if x[0] == token][0]
 
 
-def underlying_all(wallet: str, blockchain: str, decimals: bool = True) -> List[List[str]]:
-    result = get_api_results(wallet, blockchain)
-    token_claimable = {}
+def underlying_all(wallet: str, blockchain: str, decimals: bool = True) -> Dict(str,List[Dict]):
+    api_result = get_api_results(wallet, blockchain)
+    result = {'protocol':'HiddenHand',
+                        'block':'latest',
+                        'positions':[]
+                    }
+    for entry in api_result:
+        position = next((p for p in result['positions'] if p['position ID'] == entry['protocol']), None)
+        
+        if not position:
+            position = {
+                'position ID': entry['protocol'],
+                'balances': []
+            }
+            result['positions'].append(position)
+        
+        token_decimals = entry['decimals'] if not decimals else 0
+        claimable = Decimal(entry['claimable'])
+        claimable *= Decimal(10 ** token_decimals)    
 
-    for d in result:
-        token = d['token']
-        claimable = float(d['claimable'])
-        if not decimals:
-            claimable = claimable * 10**d['decimals']
-        if token in token_claimable:
-            token_claimable[token] += claimable
-        else:
-            token_claimable[token] = claimable
-
-    return [[k, str(v)] for k, v in token_claimable.items()]
+        balance = {
+            'token': entry['token'],
+            'balance': claimable
+        }
+        position['balances'].append(balance)
+    return result        
