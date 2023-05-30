291127ff36709e1bebf8fe31e260cdb9cd88009b
fito <31478879+ph1t0@users.noreply.github.com>
2023-05-30 14:07:01 -0300

    fixes pytest debug option

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,16 +3,11 @@ import pytest
 
 from defi_protocols import add_stderr_logger
 
-def pytest_configure(config):
-    config.run_extra_code_executed = False
-
 def pytest_addoption(parser):
-    parser.addoption("--log-stderr", action="store_true", help="Setup a logging handler that logs to stderr.")
+    parser.addoption("--debug-defiproto", action="store_true", help="Setup a logging handler that logs to stderr.")
 
-@pytest.fixture(autouse=True)
-def log_stderr(request):
-    if not request.config.run_extra_code_executed:
-        if request.config.getoption("--log-stderr"):
-            add_stderr_logger(logging.DEBUG)
-    request.config.run_extra_code_executed = True
+@pytest.fixture(autouse=True, scope='session')
+def debug_defi_proto(request):
+    if request.config.getoption("--debug-defiproto"):
+        add_stderr_logger(logging.DEBUG)
 
