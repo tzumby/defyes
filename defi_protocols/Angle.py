06df080c965ebd3decb9b55da1159e1584c0fa04
fito <31478879+ph1t0@users.noreply.github.com>
2023-06-13 17:22:14 -0300

    handles chain calls with an Object

--- defi_protocols/Angle.py
+++ defi_protocols/Angle.py
@@ -16,48 +16,51 @@ from defi_protocols.functions import get_node, get_contract
 logger = logging.getLogger(__name__)
 
 
-class DefiContract:
-    ABI = 'MUST_BE_DEFINED'
 
-    def __init__(self, blockchain, address):
+class ContractFunction:
+    def __init__(self, attr, args, kwargs, get_contract_func):
+        self.attr = attr
+        self.args = args
+        self.kwargs = kwargs
+        self.get_contract = get_contract_func
+
+    def _get_contract_func(self, attr, *args, **kwargs):
+        block = kwargs.get('block_identifier', 'latest')
+        contract = self.get_contract(block)
+        contract_func = getattr(contract.functions, attr)
+        return contract_func
+
+    def call(self, *args, **kwargs):
+        contract_func = self._get_contract_func(self.attr, *args, **kwargs)
+        return contract_func(*self.args, **self.kwargs).call(*args, **kwargs)
+
+    def const_call(self, *args, **kwargs):
+        contract_func = self._get_contract_func(self.attr, *args, **kwargs)
+        return const_call(contract_func(*self.args, **self.kwargs))
+
+
+class DefiContract:
+    ABI: str = 'MUST_BE_DEFINED'
+
+    def __init__(self, blockchain: str, address: str) -> None:
         self.blockchain = blockchain
         self.address = address
-        self.contract_functions = [func['name'] for func in json.loads(self.ABI)]
         self.node = None
         self.contract = None
 
-        self._func_data = None
+        for method_name in [func['name'] for func in json.loads(self.ABI)]:
+            setattr(self, method_name, self.create_method(method_name))
 
-    def _update_node_and_contract(self, block):
+    def create_method(self, method_name):
+        def method(*args, **kwargs):
+            return ContractFunction(method_name, args, kwargs, get_contract_func=self.get_contract)
+        return method
+
+    def get_contract(self, block) -> Contract:
         if self.node is None or block != self.node._called_with_block:
             self.node = get_node(self.blockchain, block)
             self.contract = self.node.eth.contract(address=self.address, abi=self.ABI)
-
-    def _get_contract_func(self, *args, **kwargs):
-        block = kwargs.get('block_identifier', 'latest')
-        self._update_node_and_contract(block)
-        contract_fun = getattr(self.contract.functions, self._func_data['name'])
-        return contract_fun
-
-    def call(self, *args, **kwargs):
-        contract_fun = self._get_contract_func(*args, **kwargs)
-        return contract_fun(*self._func_data['args'], **self._func_data['kwargs']).call(*args, **kwargs)
-
-    def const_call(self, *args, **kwargs):
-        contract_fun = self._get_contract_func(*args, **kwargs)
-        return const_call(contract_fun(*self._func_data['args'], **self._func_data['kwargs']))
-
-    def handle_contract_function(self, attr, args, kwargs):
-        self._func_data = {'name': attr, 'args': args, 'kwargs': kwargs}
-        return self
-
-    def __getattr__(self, attr):
-        if attr in self.contract_functions:
-            def aux(*args, **kwargs):
-                return self.handle_contract_function(attr, args, kwargs)
-            return aux
-        else:
-            raise AttributeError(f"'{self.__class__.__name__}' Object has no attribute '{attr}'")
+        return self.contract
 
 
 # Borrow Module
--- tests/test_angle.py
+++ tests/test_angle.py
@@ -25,5 +25,5 @@ def test_angle_treasury():
             '0x913E8e1eD659C27613E937a6B6119b91D985094c',
             '0x96de5c30F2BF4683c7903F3e921F720602F8868A'] == t.get_all_vault_managers_addrs(block)
 
-    ## everything below is WIP
+    # everything below is WIP
     Angle.get_managers_and_vaultcounts(ETHEREUM, wallet, block)
