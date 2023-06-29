94a2a3fc8c089d25aca767e6219afab25ef6a77b
santi <santiago@karpatkey.com>
2023-06-29 12:29:51 +0100

    Fixed small bug

--- defi_protocols/eth_derivs.py
+++ defi_protocols/eth_derivs.py
@@ -27,17 +27,21 @@ class EthDerivative:
         # AETH_R16: 0x1E5e5CF3652989A57736901D95749A326F5Cb60F
         # AETH_R17: 0x89632e27427109d64fFe1CdD98027139477E020F
         # AETH_R18: 0x3eD1DFBCCF893b7d2D730EAd3e5eDBF1f8f95a48
-        if self.addr == "0xE95A203B1a91a908F9B9CE46459d101078c2c3cb" and block < 16476340:
-            contract = web3.eth.contract(
-                address=self.addr,
-                abi='[{"inputs":[],"name":"ratio","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"}]',
-            )
-            eth_value = int(
-                Decimal(amount * (10**18)) / Decimal(contract.functions.ratio().call(block_identifier=block))
-            )
-        else:
+        if isinstance(block, str):
             contract = web3.eth.contract(address=self.addr, abi=self.eth_value_abi)
             eth_value = contract.functions[self.eth_value_function](amount).call(block_identifier=block)
+        else:
+            if self.addr == "0xE95A203B1a91a908F9B9CE46459d101078c2c3cb" and block < 16476340:
+                contract = web3.eth.contract(
+                    address=self.addr,
+                    abi='[{"inputs":[],"name":"ratio","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"}]',
+                )
+                eth_value = int(
+                    Decimal(amount * (10**18)) / Decimal(contract.functions.ratio().call(block_identifier=block))
+                )
+            else:
+                contract = web3.eth.contract(address=self.addr, abi=self.eth_value_abi)
+                eth_value = contract.functions[self.eth_value_function](amount).call(block_identifier=block)
         eth_value = Decimal(eth_value) / Decimal(10**token_decimals)
         return to_token_amount(self.addr, amount=eth_value, blockchain=self.blockchain, web3=web3, decimals=True)
 
