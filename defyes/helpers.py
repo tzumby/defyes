2a1748996109579c0311fb50b2ae2b852ee5aea9
Mauro Bruni <mauro@ektocomms.space>
2023-07-11 17:13:10 -0300

    Make suppression more explicit, add listify

--- defyes/helpers.py
+++ defyes/helpers.py
@@ -1,4 +1,6 @@
-from contextlib import contextmanager
+from contextlib import contextmanager, suppress
+from functools import wraps
+from typing import Any
 
 from web3.exceptions import BadFunctionCallOutput, ContractLogicError
 
@@ -6,16 +8,31 @@ suppressed_error_codes = {-32000, -32015}
 
 
 @contextmanager
-def suppress_some_contract_method_errors():
+def suppress_error_codes():
     try:
         yield
-    except (ContractLogicError, BadFunctionCallOutput):
-        pass
     except ValueError as e:
         if e.args[0]["code"] not in suppressed_error_codes:
             raise
 
 
-def call_contract_method(method, block):
-    with suppress_some_contract_method_errors():
+def call_contract_method(method, block) -> Any | None:
+    with suppress(ContractLogicError, BadFunctionCallOutput), suppress_error_codes():
         return method.call(block_identifier=block)
+
+
+def listify(func):
+    """
+    Decorator to try to cast the returned iterable into a list.
+    Otherwise returns the original returned value on TypeError.
+    """
+
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        raw = func(*args, **kwargs)
+        try:
+            return list(raw)
+        except TypeError:
+            return raw
+
+    return wrapper
