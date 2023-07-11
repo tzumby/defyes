022fef7d09639367a6c7b2f28590e493e6956bb3
fito <31478879+ph1t0@users.noreply.github.com>
2023-07-11 11:45:57 -0300

    convert classes into functions

--- defyes/util/topic.py
+++ defyes/util/topic.py
@@ -1,29 +1,17 @@
-from dataclasses import dataclass
-
 from web3 import Web3
 from web3.types import HexBytes
 
 
-@dataclass
-class TopicCreator:
-    function: str
+def topic_creator(function: str) -> str:
+    chunks = function.split(" ")
+    types = ["bytes32", "uint256", "address", "uint48"]
+    type_list = [chunk for chunk in chunks if chunk in types]
 
-    def __repr__(self) -> str:
-        chunks = self.function.split(" ")
-        types = ["bytes32", "uint256", "address", "uint48"]
-        type_list = []
-        for chunk in chunks:
-            if chunk in types:
-                type_list.append(chunk)
-        return Web3.keccak(text="{}({})".format(chunks[0], ",".join(map(str, type_list)))).hex()
+    return Web3.keccak(text="{}({})".format(chunks[0], ",".join(map(str, type_list)))).hex()
 
 
-@dataclass
-class AddressHexor:
-    address: str
-
-    def __repr__(self) -> str:
-        return str("{}000000000000000000000000{}".format(self.address[:2], self.address[2:]))
+def address_hexor(address: str) -> str:
+    return str("{}000000000000000000000000{}".format(address[:2], address[2:]))
 
 
 def encode_address_hexor(address: str):
@@ -32,7 +20,3 @@ def encode_address_hexor(address: str):
 
 def decode_address_hexor(address: HexBytes):
     return address.hex().replace("0x000000000000000000000000", "0x")
-
-
-# wallet = AddressHexor('0x849d52316331967b6ff1198e5e32a0eb168d039d')
-# print(wallet)
