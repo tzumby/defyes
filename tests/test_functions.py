6aa4b26883a85b196fe9db4ae5656ab3a7fc88e7
EktoSAn <san@ektocomms.space>
2024-01-04 11:44:22 -0300

    tests: specify block in test_search_proxy_impl_address

--- tests/test_functions.py
+++ tests/test_functions.py
@@ -59,29 +59,29 @@ def test_search_proxy_impl_address():
     assert implementation == "0x1E5e5CF3652989A57736901D95749A326F5Cb60F"
 
     # OpenZeppelins' EIP-1167
-    implementation = search_proxy_impl_address("0x793fAF861a78B07c0C8c0ed1450D3919F3473226", Chain.GNOSIS)
+    implementation = search_proxy_impl_address("0x793fAF861a78B07c0C8c0ed1450D3919F3473226", Chain.GNOSIS, block=31782206)
     assert implementation == "0x45fFd460cC6642B8D8Fb12373DFd77Ceb0f4932B"
 
     # Custom proxy implementation (similar to EIP-1167)
-    implementation = search_proxy_impl_address("0x09cabEC1eAd1c0Ba254B09efb3EE13841712bE14", Chain.ETHEREUM)
+    implementation = search_proxy_impl_address("0x09cabEC1eAd1c0Ba254B09efb3EE13841712bE14", Chain.ETHEREUM, block=16475978)
     assert implementation == "0x2157A7894439191e520825fe9399aB8655E0f708"
 
     # OpenZeppelins' Unstructured Storage proxy pattern
-    implementation = search_proxy_impl_address(ETHTokenAddr.USDC, Chain.ETHEREUM)
+    implementation = search_proxy_impl_address(ETHTokenAddr.USDC, Chain.ETHEREUM, block=16475978)
     assert implementation == "0xa2327a938Febf5FEC13baCFb16Ae10EcBc4cbDCF"
 
     implementation = search_proxy_impl_address(ETHTokenAddr.USDC, Chain.ETHEREUM, block=10800000)
     assert implementation == "0xB7277a6e95992041568D9391D09d0122023778A2"
 
     # OpenZeppelins' EIP-897 DelegateProxy
-    implementation = search_proxy_impl_address(ETHTokenAddr.stETH, Chain.ETHEREUM)
+    implementation = search_proxy_impl_address(ETHTokenAddr.stETH, Chain.ETHEREUM, block=18934617)
     assert implementation == "0x17144556fd3424EDC8Fc8A4C940B2D04936d17eb"
 
-    implementation = search_proxy_impl_address("0x3d9819210A31b4961b30EF54bE2aeD79B9c9Cd3B", Chain.ETHEREUM)
+    implementation = search_proxy_impl_address("0x3d9819210A31b4961b30EF54bE2aeD79B9c9Cd3B", Chain.ETHEREUM, block=18934617)
     assert implementation == "0xBafE01ff935C7305907c33BF824352eE5979B526"
 
     # Custom proxy implementation (used by Safes)
-    implementation = search_proxy_impl_address("0x4F2083f5fBede34C2714aFfb3105539775f7FE64", Chain.ETHEREUM)
+    implementation = search_proxy_impl_address("0x4F2083f5fBede34C2714aFfb3105539775f7FE64", Chain.ETHEREUM, block=18934617)
     assert implementation == "0xd9Db270c1B5E3Bd161E8c8503c55cEABeE709552"
 
 
