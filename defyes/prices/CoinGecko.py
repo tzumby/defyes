43fe3db9ee4d27b448ae9a0c450a49fd1d7b70c5
fito <31478879+ph1t0@users.noreply.github.com>
2024-01-30 14:58:23 -0300

    refactors CoinGecko to comply with C901

--- defyes/prices/CoinGecko.py
+++ defyes/prices/CoinGecko.py
@@ -22,36 +22,22 @@ def get_price(token_address, timestamp, blockchain):
     :param blockchain:
     :return:
     """
+    HOURS = [(23, 1), (24 * 40, 24 * 40), (24 * 180, 24 * 180)]
     if blockchain not in [Chain.ETHEREUM, Chain.GNOSIS, Chain.POLYGON, Chain.AVALANCHE, Chain.OPTIMISM, Chain.ARBITRUM]:
         return [timestamp, None]
 
     if token_address == Address.ZERO:
-        if blockchain == Chain.POLYGON:
-            coin_id = "matic-network"
-        else:
-            coin_id = blockchain
+        coin_id = "matic-network" if blockchain == Chain.POLYGON else blockchain
 
-        data = requests.get(URL_COINID_PRICE_RANGE % (coin_id, timestamp - 3600 * 23, timestamp + 3600 * 1))
-        if data.status_code != 200:
+        for hours in HOURS:
+            data = requests.get(
+                URL_COINID_PRICE_RANGE % (coin_id, timestamp - 3600 * hours[0], timestamp + 3600 * hours[1])
+            )
+            if data.status_code == 200 and data.json()["prices"]:
+                data = data.json()["prices"]
+                break
+        if not isinstance(data, list):
             return [data.status_code, [timestamp, None]]
-        else:
-            data = data.json()["prices"]
-            if data == []:
-                data = requests.get(
-                    URL_COINID_PRICE_RANGE % (coin_id, timestamp - 3600 * 24 * 40, timestamp + 3600 * 24 * 40)
-                )
-                if data.status_code != 200:
-                    return [data.status_code, [timestamp, None]]
-                else:
-                    data = data.json()["prices"]
-                    if data == []:
-                        data = requests.get(
-                            URL_COINID_PRICE_RANGE % (coin_id, timestamp - 3600 * 24 * 180, timestamp + 3600 * 24 * 180)
-                        )
-                        if data.status_code != 200:
-                            return [data.status_code, [timestamp, None]]
-                        else:
-                            data = data.json()["prices"]
 
     else:
         blockchain_id = {
@@ -60,58 +46,40 @@ def get_price(token_address, timestamp, blockchain):
             Chain.ARBITRUM: "arbitrum-one",
         }.get(blockchain, blockchain)
 
-        data = requests.get(
-            URL_BLOCKCHAINID_TOKENADDRESS_PRICE_RANGE
-            % (blockchain_id, token_address, timestamp - 3600 * 23, timestamp + 3600 * 1)
-        )
-        if data.status_code != 200:
+        for hours in HOURS:
+            data = requests.get(
+                URL_BLOCKCHAINID_TOKENADDRESS_PRICE_RANGE
+                % (blockchain_id, token_address, timestamp - 3600 * hours[0], timestamp + 3600 * hours[1])
+            )
+            if data.status_code == 200 and data.json()["prices"]:
+                data = data.json()["prices"]
+                break
+        if not isinstance(data, list):
             return [data.status_code, [timestamp, None]]
-        else:
-            data = data.json()["prices"]
-            if data == []:
-                data = requests.get(
-                    URL_BLOCKCHAINID_TOKENADDRESS_PRICE_RANGE
-                    % (blockchain_id, token_address, timestamp - 3600 * 24 * 40, timestamp + 3600 * 24 * 40)
-                )
-                if data.status_code != 200:
-                    return [data.status_code, [timestamp, None]]
-                else:
-                    data = data.json()["prices"]
-                    if data == []:
-                        data = requests.get(
-                            URL_BLOCKCHAINID_TOKENADDRESS_PRICE_RANGE
-                            % (blockchain_id, token_address, timestamp - 3600 * 24 * 180, timestamp + 3600 * 24 * 180)
-                        )
-                        if data.status_code != 200:
-                            return [data.status_code, [timestamp, None]]
-                        else:
-                            data = data.json()["prices"]
 
     if data == [] or data is None:
         return [200, [timestamp, None]]
-    else:
-        if len(data) == 1:
-            return [200, [math.floor(data[0][0] / 1000), data[0][1]]]
-        i = 0
-        while int(data[i][0]) < timestamp * 1000:
-            i += 1
-            if i == len(data):
-                break
 
+    if len(data) == 1:
+        return [200, [math.floor(data[0][0] / 1000), data[0][1]]]
+
+    i = 0
+    while int(data[i][0]) < timestamp * 1000:
+        i += 1
         if i == len(data):
-            return [200, [math.floor(data[i - 1][0] / 1000), data[i - 1][1]]]
-        else:
-            return [
-                200,
-                [
-                    timestamp,
-                    (
-                        (timestamp * 1000 - data[i - 1][0]) * data[i][1]
-                        + (data[i][0] - timestamp * 1000) * data[i - 1][1]
-                    )
-                    / (data[i][0] - data[i - 1][0]),
-                ],
-            ]
+            break
+
+    if i == len(data):
+        return [200, [math.floor(data[i - 1][0] / 1000), data[i - 1][1]]]
+
+    return [
+        200,
+        [
+            timestamp,
+            ((timestamp * 1000 - data[i - 1][0]) * data[i][1] + (data[i][0] - timestamp * 1000) * data[i - 1][1])
+            / (data[i][0] - data[i - 1][0]),
+        ],
+    ]
 
 
 # ---------------------------------------------------------------------------------------------------------------------------------------------------------------------------
