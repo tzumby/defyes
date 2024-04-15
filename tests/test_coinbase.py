c7fed95efdd9a2cf41a0aba728fa37e0e507d86a
ubabe53 <umberto@karpatkey.com>
2024-04-15 16:30:31 +0100

    fix: wrong variables on tests

--- tests/test_coinbase.py
+++ tests/test_coinbase.py
@@ -8,10 +8,10 @@ from defyes.protocols.coinbase import reduce_cbETH
 
 
 def test_reduce_cbETH():
-    result = reduce_cbETH(10000, "ethereum", 19661532, False)
+    result = reduce_cbETH(10000, "ethereum", 19661532)
     assert result == ("0x0000000000000000000000000000000000000000", Decimal("10689.76237590522102"))
 
 
 def test_reduce_cbETH_invalid_blockchain():
     with pytest.raises(ValueError, match="Currently only Ethereum is supported"):
-        reduce_cbETH(10000, "invalid_blockchain", 19661532, False)
+        reduce_cbETH(10000, "invalid_blockchain", 19661532)
